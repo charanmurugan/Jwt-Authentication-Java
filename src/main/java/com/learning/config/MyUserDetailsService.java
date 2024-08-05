8c2c6b23ab13376fbb0dd0643c2d2b7f89637d6b
Charan_M <murugancharan245@gmail.com>
2024-08-05 21:51:18 +0530

    fixed the error while compilation

--- src/main/java/com/learning/config/MyUserDetailsService.java
+++ src/main/java/com/learning/config/MyUserDetailsService.java
@@ -8,9 +8,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
 import java.util.Optional;
-
+@Service
 public class MyUserDetailsService implements UserDetailsService {
 
     public UserDetailsRepository userDetailsRepository;
